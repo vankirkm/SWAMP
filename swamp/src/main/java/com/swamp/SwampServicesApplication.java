f887d21c346bb0c69842ffeb8de01af77b353967
Michael Van Kirk <vankirk.6@wright.edu>
2023-01-24 09:08:18 -0500

    updating submodules

---
+++ swamp/src/main/java/com/swamp/SwampServicesApplication.java
@@ -0,0 +1,13 @@
+package com.swamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwampServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwampServicesApplication.class, args);
+	}
+
+}
---
+++ swamp/src/test/java/com/swamp/SwampServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwampServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
